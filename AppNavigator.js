5934a83d8a1e01c4904641800af4beb33cbbe71f
sombrilla <juaan.beldi@hotmail.com>
2019-05-12 14:20:54 -0300

    Add nba page

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,8 +1,10 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import Home from './containers/home/home.js';
+import Home from './containers/home/home';
+import Nba from './containers/home/home';
 
 const AppNavigator = createStackNavigator({
   Home: { screen: Home },
+  Nba: { screen: Nba },
 });
 
 const AppContainer = createAppContainer(AppNavigator);
---
+++ containers/nba/nba.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { View } from 'react-native';
+import main from './styles';
+
+export default class Nba extends React.Component {
+  render() {
+    
+    return (
+      <View style={main.container} />
+    );
+  }
+}
+
+// Nba.propTypes = {}
+
+// Nba.defaultProps = {}
+
+
