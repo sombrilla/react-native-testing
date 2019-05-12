bc09db84f951c7437833c31745dd697c5b77a914
sombrilla <juaan.beldi@hotmail.com>
2019-05-12 15:54:45 -0300

    Add event page

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,10 +1,12 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Home from './containers/home/home';
 import Nba from './containers/nba/nba';
+import Event from './containers/event/event';
 
 const AppNavigator = createStackNavigator({
   Home: { screen: Home },
   Nba: { screen: Nba },
+  Event: { screen: Event },
 });
 
 const AppContainer = createAppContainer(AppNavigator);
---
+++ containers/event/event.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { View } from 'react-native';
+import main from './styles';
+
+export default class Event extends React.Component {
+  render() {
+    
+    return (
+      <View style={main.container} />
+    );
+  }
+}
+
+// Event.propTypes = {}
+
+// Event.defaultProps = {}
+
+
